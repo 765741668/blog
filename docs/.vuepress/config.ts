d20293ec3736668faefb07d8b6895a55a9e6fed2
Yzh <765741668@qq.com>
2022-10-24 18:34:37 +0800

    banner图片2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -150,21 +150,21 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
 
     // pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
 
-    bodyBgImg: [
-      'http://lsjqn.orochi.press/blog/创生之柱.jpg',
-      'http://lsjqn.orochi.press/blog/蟹状星云2.jpg',
-      'http://lsjqn.orochi.press/blog/蝴蝶星云.jpg',
-      'http://lsjqn.orochi.press/blog/玫瑰星系.jpg',
-      'http://lsjqn.orochi.press/blog/黑洞.jpg',
-    ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时隔bodyBgImgInterval切换一张。
-    bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~1.0, 默认0.5
-    bodyBgImgInterval: 15, // body多张背景图时的切换间隔, 默认15，单位s
-    titleBadge: false, // 文章标题前的图标是否显示，默认true
-    titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
-      '图标地址1',
-      '图标地址2'
-    ],
-    contentBgStyle: 1, // 文章内容块的背景风格，默认无. 1 方格 | 2 横线 | 3 竖线 | 4 左斜线 | 5 右斜线 | 6 点状
+    // bodyBgImg: [
+    //   'http://lsjqn.orochi.press/blog/创生之柱.jpg',
+    //   'http://lsjqn.orochi.press/blog/蟹状星云2.jpg',
+    //   'http://lsjqn.orochi.press/blog/蝴蝶星云.jpg',
+    //   'http://lsjqn.orochi.press/blog/玫瑰星系.jpg',
+    //   'http://lsjqn.orochi.press/blog/黑洞.jpg',
+    // ], // body背景大图，默认无。 单张图片 String | 多张图片 Array, 多张图片时隔bodyBgImgInterval切换一张。
+    // bodyBgImgOpacity: 0.5, // body背景图透明度，选值 0.1~1.0, 默认0.5
+    // bodyBgImgInterval: 15, // body多张背景图时的切换间隔, 默认15，单位s
+    // titleBadge: false, // 文章标题前的图标是否显示，默认true
+    // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
+    //   '图标地址1',
+    //   '图标地址2'
+    // ],
+    // contentBgStyle: 1, // 文章内容块的背景风格，默认无. 1 方格 | 2 横线 | 3 竖线 | 4 左斜线 | 5 右斜线 | 6 点状
 
     updateBar: { // 最近更新栏
       showToArticle: true, // 显示到文章页底部，默认true
